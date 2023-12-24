072ffc7d3a813f99e78b028a709559110bd35808
elnuritos <elnu1990@list.ru>
2023-12-25 00:06:23 +0100

    hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Publications } from './components/Publications';
-import Navbar from './components/Navbar';
+import {Navbar} from './components/Navbar';
 
 function App() {
   return (
     <div>
-    <Navbar />
-    <main>
+ 
+
+ 
       <Publications />
-    </main>
+
   </div>
   );
 }
